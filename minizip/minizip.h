18d323b079c79b02dd12fe58f44e915ffefbf39f
Shota Shimazu <hornet.live.mf@gmail.com>
2018-12-10 02:30:34 +0900

    revise

---
+++ minizip/minizip.h
@@ -0,0 +1,19 @@
+//
+//  minizip.h
+//  minizip
+//
+//  Created by Shota Shimazu on 2018/12/10.
+//  Copyright © 2018 Shota Shimazu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for minizip.
+FOUNDATION_EXPORT double minizipVersionNumber;
+
+//! Project version string for minizip.
+FOUNDATION_EXPORT const unsigned char minizipVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <minizip/PublicHeader.h>
+
+
